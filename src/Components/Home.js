8fb4b327b41ace3369cd72257120f0ecef8eca85
haaffin <eastonahaaff1998@gmail.com>
2021-10-11 17:00:47 -0500

    began adjusting mobile view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,10 +17,10 @@ function Home(){
 
             <div className='main_body'>
                 <div className='button1'>
-                <Link to='/usercard'> <Button variant="danger"> Meet the gang! </Button> </Link>
+                <Link to='/usercard'> <Button variant="primary"> Meet the gang! </Button> </Link>
                 </div>
                 <div className='button2'>
-                    <a href='https://forms.gle/rcyPXQMaGQGKBZNm9' target='__blank'><Button variant="danger"> Apply to join! </Button></a>
+                    <a href='https://forms.gle/rcyPXQMaGQGKBZNm9' target='__blank'><Button variant="primary"> Apply to join! </Button></a>
                 </div>
             </div>
 
