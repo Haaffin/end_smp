87838721a36b2dce678d51ca19f72b38876c98aa
Echo <eeecho1998@gmail.com>
2021-10-20 15:37:54 -0500

    changed about message to remove cringy lorem ipsum

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,10 +8,8 @@ function Home(){
         <div className='home_contain'>
             <div className="about">
             <h1>About The End</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate mi eleifend turpis mollis pellentesque. 
-                    Integer tempus placerat ex sit amet vulputate. Suspendisse elementum risus sem, eget scelerisque ligula pellentesque at. 
-                    Sed et ultrices augue. Proin blandit justo non felis dignissim consectetur. Nulla malesuada varius urna, a egestas neque lacinia nec. 
-                    Phasellus commodo non massa a dictum. Praesent convallis neque vel nisl vulputate semper eu eu nulla. Maecenas ut sem ligula.
+                <p>
+                    About Us coming soon
                 </p>
             </div>
 
